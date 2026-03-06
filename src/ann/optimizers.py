55971927537905affe8da4aafca9596be433a4fb
Abdallah <zda23b014@iitmz.ac.in>
2026-03-06 22:01:01 +0300

    added SGD optimizer

--- src/ann/optimizers.py
+++ src/ann/optimizers.py
@@ -2,3 +2,15 @@
 Optimization Algorithms
 Implements: SGD, Momentum, Adam, Nadam, etc.
 """
+import numpy as np
+
+class SGD:
+    def __init__(self, learning_rate=0.01):
+        self.lr = learning_rate
+
+    def update(self, layer):
+        if hasattr(layer, "W"):
+            layer.W = layer.W - self.lr * layer.grad_W
+
+        if hasattr(layer, "b"):
+            layer.b = layer.b - self.lr * layer.grad_b
